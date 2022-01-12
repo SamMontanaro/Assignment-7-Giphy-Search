4d0b4803defc40f88dc1725658210ac4fd1b0df8
Sam Montanaro <sammontanaro12@gmail.com>
2022-01-11 22:14:34 -0500

    Added rating filter for searches

--- src/Components/SearchField.jsx
+++ src/Components/SearchField.jsx
@@ -13,7 +13,7 @@ class SearchField extends React.Component {
         let input = document.getElementById("gif").value;
         
         await axios
-          .get("http://api.giphy.com/v1/gifs/search?q=" + input + "&api_key=4Uqlux8KuvAH0FneRmAkDpYXWZsLC8rm")
+          .get(`http://api.giphy.com/v1/gifs/search?q=${input}&api_key=4Uqlux8KuvAH0FneRmAkDpYXWZsLC8rm&rating=${document.getElementById("rating").value}`)
           .then(response => {
             this.state.gifs = response.data.data;
           })
@@ -25,6 +25,11 @@ class SearchField extends React.Component {
         return (
             <div className='d-flex flex-row justify-content-center align-items-center'>
                 <div className='d-flex justify-content-center my-3'>
+                    <select className="form-select w-50" id='rating'>
+                        <option value="g">Rated G</option>
+                        <option value="pg">Rated PG</option>
+                        <option value="pg-13">Rated PG-13</option>
+                    </select>
                     <input type="text" id="gif" className="form-control text-center"/>
                     <button type="submit" className='btn btn-warning px-3' onClick={this.fetchGifs}>Search</button>
                 </div>
